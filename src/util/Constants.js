3b35aa6934ddd9be9713d51df2623f3dba159f2d
DEFAULT <61415181+DEF4ULT-OP@users.noreply.github.com>
2020-09-26 15:36:08 +0530

    Update Constants.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -5,14 +5,14 @@
 
 export.DefaultOptions = {
   cacheMessages: true,
-  debug: false,
+  debug: false
 };
   /**
-   * polling options
-   * @typedeaf {Object} pollingOptions
-   * @property {boolean} [enabled=true] Whether to enable polling
-   * @property {number} [interval=300] Interval for polling updates from telegram (in ms - do not set too low interval)
-   *
+   polling options
+    @typedeaf {Object} pollingOptions
+    @property {boolean} [enabled=true] Whether to enable polling
+   @property {number} [interval=300] Interval for polling updates from telegram (in ms - do not set too low interval)
+   
   polling: {
     enabled: true,
     interval: 300,
