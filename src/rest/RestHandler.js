83c0105cac47c0b40305e7dc00e5368b22d64889
DEF4ULT-OP <bodforbod@gmail.com>
2020-09-26 12:18:09 +0530

    Handle rest and request

---
+++ src/rest/RestHandler.js
@@ -0,0 +1,22 @@
+const Request = require('./RequestHandler');
+
+ class RestHandler {
+   constructor(client) {
+     this.client = client;
+     this.handler = new Request(client);
+     
+   }
+   
+   get(path, options) {
+     options.method = "GET";
+     this.handler.request(path, options)
+   }
+   
+   post(path, options) {
+     options.method = 'POST';
+     this.handler.request(path, options);
+   }
+    
+ }
+ 
+ module.exports = RestHandler;
