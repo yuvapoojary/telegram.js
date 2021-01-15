8039739b963b41fe2ebcdb67590f02f250a90a8e
yuvapoojary <bodforbod@gmail.com>
2021-01-15 16:07:28 +0530

    log

--- src/rest/RESTManager.js
+++ src/rest/RESTManager.js
@@ -1,36 +1,37 @@
 const APIRouter = require('./APIRouter');
 const APIRequest = require('./APIRequest');
 
- class RESTManager {
-   constructor(client) {
-     this.client = client;
-   }
-   
-   get api() {
-     return APIRouter(this);
-   };
-   
-   get endpoint() {
-     return this.client.options.ApiURL;
-   };
-   
-   async request(method, path, options) {
-     const handler = new APIRequest(this, method, path, options);
-     let json;
-     const res = await handler.make();
-     try {
-       json = await res.json();
-     } catch(e) {
-       throw new Error('Unable to parse api response');
-     };
-     if(res.ok) return json;
-     return Promise.reject(json);
-   };
-   
-   getAuth() {
-     return `bot${this.endpoint}`;
-   };
-    
- }
- 
- module.exports = RESTManager;
+class RESTManager {
+  constructor(client) {
+    this.client = client;
+  }
+
+  get api() {
+    return APIRouter(this);
+  };
+
+  get endpoint() {
+    return this.client.options.ApiURL;
+  };
+
+  async request(method, path, options) {
+    const handler = new APIRequest(this, method, path, options);
+    let json;
+    const res = await handler.make();
+    try {
+      json = await res.json();
+    } catch (e) {
+      throw new Error('Unable to parse api response');
+    };
+    console.log(json)
+    if (res.ok) return json;
+    return Promise.reject(json);
+  };
+
+  getAuth() {
+    return `bot${this.endpoint}`;
+  };
+
+}
+
+module.exports = RESTManager;
